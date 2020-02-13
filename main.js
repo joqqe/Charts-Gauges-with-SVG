94b1a50096b32509baac3bfc070bc77c74925ace
Jochen Schoonjans <jochenschoonjans@hotmail.com>
2020-02-13 20:04:31 +0100

    Small improvements

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var weatherIcons = [
     { icon: "sun", description: "Sun" },
     { icon: "sun-overcast", description: "Slightly overcast" },
-    { icon: "sun-overcast-rain", description: "Slightly overcast with rain" },
+    { icon: "sun-overcast-rain", description: "Rain" },
     { icon: "overcast", description: "Overcast" },
     { icon: "overcast-rain", description: "Overcast with rain" },
     { icon: "thunderstorm", description: "Thunderstorm" },
@@ -9,6 +9,10 @@ var weatherIcons = [
     { icon: "snow", description: "Snow" },
     { icon: "hail", description: "Hail" }
 ]
+
+var mapValueToScale = function(value, oldMin, oldMax, newMin, newMax) {
+    return (newMax - newMin) - (value - oldMin) * (newMax - newMin) / (oldMax - oldMin) + newMin;
+}
 
 var updateWeather = function (icon, description) {
     var weatherIcon = document.querySelector("#weather-icon use");
@@ -26,7 +30,7 @@ var updateTemperature = function (temperature) {
     var maxTemp = 30;
     var minTemp = 0;
     var tempIconC = tempIcon.getBBox();
-    var tempIconXOrigin = tempIconC.x + tempIconC.width;
+    var tempIconXOrigin = tempIconC.x + (tempIconC.width / 2);
     var tempIconYOrigin = tempIconC.y + tempIconC.height;
     var tempPercentage = temperature / (maxTemp - minTemp);
     var scalePercentage = (tempPercentage / 1.2) + (1 - (1 / 1.2));
@@ -67,19 +71,19 @@ var updateChart = function (data, type) {
     // Clear
     forecastChart.innerHTML = "";
 
-    var viewBox = forecastChart.viewBox.baseVal;
-    var chartPosition = { xMin: 45, xMax: viewBox.width - 15, yMin: 15, yMax: viewBox.height - 45 };
+    var viewBox = forecastChart.viewBox.baseVal,
+        chartPosition = { xMin: 45, xMax: viewBox.width - 15, yMin: 15, yMax: viewBox.height - 45 },
         xSubdivide = data.length,
         ySubdivide = 5,
-        scale = { min: Math.floor(Math.min(...data.map(x => x.value)) / 5) * 5, max: Math.ceil(Math.max(...data.map(x => x.value)) / 5) * 5 };
-    columnWidth = (chartPosition.xMax - chartPosition.xMin) / xSubdivide;
+        scale = { min: Math.floor(Math.min(...data.map(x => x.value)) / 5) * 5, max: Math.ceil(Math.max(...data.map(x => x.value)) / 5) * 5 },
+        columnWidth = (chartPosition.xMax - chartPosition.xMin) / xSubdivide;
 
     for (let i = 0; i < ySubdivide; i++) {
         // Horizontal lines
-        var path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-        var x = chartPosition.xMin;
-        var y = (i - 0) * (chartPosition.yMax - chartPosition.yMin) / ((ySubdivide - 1) - 0) + chartPosition.yMin;
-        var l = chartPosition.xMax - chartPosition.xMin;
+        var path = document.createElementNS("http://www.w3.org/2000/svg", "path"),
+            x = chartPosition.xMin,
+            y = mapValueToScale(i, 0, (ySubdivide - 1), chartPosition.yMin, chartPosition.yMax),
+            l = chartPosition.xMax - chartPosition.xMin;
 
         path.style.stroke = "909090";
         path.style.strokeWidth = "0.5";
@@ -91,9 +95,9 @@ var updateChart = function (data, type) {
         // Y axis
         var text = document.createElementNS("http://www.w3.org/2000/svg", "text");
         var x = chartPosition.xMin - 40;
-        var y = (chartPosition.yMax - chartPosition.yMin) - (i - 0) * (chartPosition.yMax - chartPosition.yMin) / ((ySubdivide - 1) - 0) + chartPosition.yMin;
+        var y = mapValueToScale(i, 0, (ySubdivide - 1), chartPosition.yMin, chartPosition.yMax);
 
-        text.textContent = ((scale.max - scale.min) / (ySubdivide - 1)) * i + scale.min  + type;
+        text.textContent = mapValueToScale(i, 0, (ySubdivide - 1), scale.max, scale.min) + type;
         text.setAttribute("font-size", "10");
         text.setAttribute("x", x);
         text.setAttribute("y", y + 3.5);
@@ -125,26 +129,24 @@ var updateChart = function (data, type) {
 
         // Line
         var x = (chartPosition.xMin + columnWidth / 2) + (columnWidth * i);
-        var y = (chartPosition.yMax - chartPosition.yMin) - (data[i].value - scale.min) * (chartPosition.yMax - chartPosition.yMin) / (scale.max - scale.min) + chartPosition.yMin;
+        var y = mapValueToScale(data[i].value, scale.min, scale.max, chartPosition.yMin, chartPosition.yMax);
 
-        if (i === 0) {
-            d += x + " " + y;
-        } else {
-            d += " L " + x + " " + y;
-        }
+        if (!i) d += x + " " + y;
+        else    d += " L " + x + " " + y;
 
+        // Dot
         var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
         circle.setAttribute("r", 2.5);
         circle.setAttribute("cx", x);
         circle.setAttribute("cy", y);
 
+        // Hover
         var title = document.createElementNS("http://www.w3.org/2000/svg", "title");
         title.textContent = data[i].date + "\n" + data[i].value + type;
 
         circle.append(title);
         forecastChart.append(circle);
     }
-
     path.setAttribute("d", d);
     forecastChart.prepend(path);
 }
@@ -169,4 +171,4 @@ init();
 
 setInterval(() => {
     init();
-}, 5000);
+}, 4000);
